ab7ebe1e83715797f14d5e5b1b1d3bb0eccebc88
Elizabeth Ornelas <eliornelasvillal35@gmail.com>
2026-09-11 20:47:49 -0600

    Heroes Html 11/09/2026

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,6 +6,20 @@ import { Component, signal } from '@angular/core';
   standalone: false,
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('IntroAngular');
+
+  titulo:string="ELIZABETH ORNELAS GOMEZ"
+
+  duplicaNumero(num:number):number{
+    return num*2
+  }
+  pelicula={
+    titulo:"Barbie",
+    anio:2024,
+    genero:"Comedia",
+    fechaLanzamiento:new Date(),
+    precio:345
+
+  }
 }
